196f195520a6f17fa93114b6c896af73b94dfafa
abdurrashidkhan <a.rashidkhan.dev@gmail.com>
2026-01-28 14:59:15 +0600

    mitarial ui init

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ButtonUsage from './Components/button/Button';
+
+const Home = () => {
+  return (
+    <div>
+      <ButtonUsage />
+    </div>
+  );
+};
+
+export default Home;
