47a79b4941f4b8147f9f581bcec8da4bd560d418
Meeherab <meherabhasan.nu@gmail.com>
2026-02-13 16:27:25 +0600

    update

--- src/Components/Navbar/types.ts
+++ src/Components/Navbar/types.ts
@@ -1,25 +1,25 @@
-export type SubItem = {
-  label: string
-  href: string
-}
+    export type SubItem = {
+    label: string
+    href: string
+    }
 
-export type NavItem = {
-  label: string
-  href?: string
-  subItems?: SubItem[]
-}
+    export type NavItem = {
+    label: string
+    href?: string
+    subItems?: SubItem[]
+    }
 
-export type NavbarButton = {
-  label: string
-  href: string
-  className?: string
-}
+    export type NavbarButton = {
+    label: string
+    href: string
+    className?: string
+    }
 
-export interface NavbarProps {
-  logo: React.ReactNode
-  items: NavItem[]
-  buttons?: NavbarButton[]
-  className?: string
-  mobileClassName?: string
-  onLinkClick?: (href: string) => void
-}
+    export interface NavbarProps {
+    logo: React.ReactNode
+    items: NavItem[]
+    buttons?: NavbarButton[]
+    className?: string
+    mobileClassName?: string
+    onLinkClick?: (href: string) => void
+    }
